8d18361581bda2143693569e0e531a459f59fb1d
slushey <42842766+slushey@users.noreply.github.com>
2018-09-05 18:22:37 -0400

    Updated from Brython Server: 9/5/2018 6:22:36 PM

--- picture.py
+++ picture.py
@@ -46,7 +46,7 @@ rectangle2 = RectangleAsset(10, 160, line, pink)
 rectangle3 = RectangleAsset(140, 140, line, lightpink)
 face = CircleAsset(30, line2, yellow)
 cheeks = CircleAsset(5, line2, red)
-ears = PolygonAsset([(60,0), (20,10), (0,55),(53,20), (60,0)], line2, yellow)
+ears = PolygonAsset([(60,0), (40,7), (0,55),(53,20), (60,0)], line2, yellow)
 ears2 = PolygonAsset([(0,0), (7,20), (60,55),(20,7), (0,0)], line2, yellow)
 
 #stamp background and border
@@ -57,7 +57,7 @@ Sprite(rectangle2, (230,100))
 Sprite(rectangle3, (90,110))
 
 #pikachu
-Sprite(ears)
+Sprite(ears, (160,125))
 Sprite(ears2, (100,125))
 Sprite(face, (130,150))
 Sprite(cheeks, (135,180))
