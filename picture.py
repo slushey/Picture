4c37fdd6774a61ed71b64f915d059b1f108a9de3
slushey <42842766+slushey@users.noreply.github.com>
2018-09-05 18:28:23 -0400

    Updated from Brython Server: 9/5/2018 6:28:23 PM

--- picture.py
+++ picture.py
@@ -39,6 +39,7 @@ lightpink = Color(0xffe6ee, 1.0)
 #lines used
 line = LineStyle(0.2, pink)
 line2 = LineStyle(0.2, brown)
+line3 = LineStyle(0.2, black)
 
 #shapes used
 rectangle = RectangleAsset(160, 10, line, pink)
@@ -48,6 +49,8 @@ face = CircleAsset(30, line2, yellow)
 cheeks = CircleAsset(5, line2, red)
 ears = PolygonAsset([(60,0), (40,7), (0,55),(53,20), (60,0)], line2, yellow)
 ears2 = PolygonAsset([(0,0), (7,20), (60,55),(20,7), (0,0)], line2, yellow)
+tip = PolygonAsset([(60,0), (40,7), (53, 20), (60,0)], line3, black)
+tip2 = PolygonAsset([(0,0), (7,20), (20,7), (0,0)], line3, black)
 
 #stamp background and border
 Sprite(rectangle, (80, 100))
@@ -62,6 +65,8 @@ Sprite(ears2, (100,125))
 Sprite(face, (130,150))
 Sprite(cheeks, (135,180))
 Sprite(cheeks, (175,180))
+Sprite(tip, (200,125))
+Sprite(tip2, (100,125))
 
 # add your code here /\  /\  /\
 
